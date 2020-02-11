3fafcf38c423b73b9723e5880cf3c1246b71f443
Guillermo Serrato <Guillermo Serrato@192.168.0.13>
2020-02-11 15:05:45 -0500

    Ejercicio 5 subido

--- MendozaAlfonso_SerratoRamirez_Taller2/src/co/edu/unbosque/controller/AplMain_Ejercicio5.java
+++ MendozaAlfonso_SerratoRamirez_Taller2/src/co/edu/unbosque/controller/AplMain_Ejercicio5.java
@@ -1,5 +1,20 @@
 package co.edu.unbosque.controller;
 
-public class AplMain_Ejercicio5 {
+import java.util.Scanner;
 
+public class AplMain_Ejercicio5 {
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		String palabra;
+		String palabrainvertida = "";
+		System.out.println("Ingresa una palabra y se mostrara a la inversa:\n");
+		palabra = s.nextLine();
+		
+		for (int i = palabra.length()-1; i >= 0; i--) {
+			palabrainvertida = palabrainvertida + palabra.charAt(i);
+		}
+
+		System.out.println("La palabra invertida es:\n" + palabrainvertida);
+	}
 }
--- MendozaAlfonso_SerratoRamirez_Taller2/src/co/edu/unbosque/model/Ejercicio5.java
+++ MendozaAlfonso_SerratoRamirez_Taller2/src/co/edu/unbosque/model/Ejercicio5.java
@@ -2,4 +2,29 @@ package co.edu.unbosque.model;
 
 public class Ejercicio5 {
 
+	private String palabra;
+	private String palabrainvertida = "";
+	
+	Ejercicio5(){
+		
+	}
+
+	public String getPalabra() {
+		return palabra;
+	}
+
+	public void setPalabra(String palabra) {
+		this.palabra = palabra;
+	}
+
+	public String getPalabrainvertida() {
+		return palabrainvertida;
+	}
+
+	public void setPalabrainvertida(String palabrainvertida) {
+		this.palabrainvertida = palabrainvertida;
+	}
+	
+	
+	
 }
