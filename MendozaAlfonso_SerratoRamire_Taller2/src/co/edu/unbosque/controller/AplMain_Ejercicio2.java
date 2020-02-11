5f48948d379de3310536976b581426d931a679db
aulamovil08 <aulamovil08@UBAULAM01-05.salas.edu.co>
2020-02-11 10:43:43 -0500

    Se agrego la realización del Ejercicio 2

--- MendozaAlfonso_SerratoRamire_Taller2/src/co/edu/unbosque/controller/AplMain_Ejercicio2.java
+++ MendozaAlfonso_SerratoRamire_Taller2/src/co/edu/unbosque/controller/AplMain_Ejercicio2.java
@@ -1,5 +1,27 @@
 package co.edu.unbosque.controller;
 
+import java.util.Random;
+
+import co.edu.unbosque.model.Ejercicio2;
+
 public class AplMain_Ejercicio2 {
 
+	public static void main(String[] args) {
+		
+		Ejercicio2 x = new Ejercicio2();
+		Ejercicio2 principal = new Ejercicio2();
+		Runtime runtime = Runtime.getRuntime();
+		int procesadoresLogicos = runtime.availableProcessors();
+
+		Random rand = new Random(System.nanoTime());
+		
+		int[][] matriz = principal.getMatriz();
+		
+		for (int i = principal.getIni(); i < principal.getFin(); i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				matriz[i][j] = rand.nextInt(10);
+			}
+		}
+	}
+	
 }
